4b6095fad14bf96a7c981271b93b6378190e6084
Pony <pony@unnotech.com>
2025-07-13 16:53:51 +0800

    feat(celery): enable SSL thread pool and conditional TLS config

--- entrypoint-celery.sh
+++ entrypoint-celery.sh
@@ -3,4 +3,4 @@
 
 
 QUEUE=${1:-playlog_q}
-celery -A koala worker -Q $QUEUE --concurrency=1 --loglevel=info -n ${QUEUE}_worker@%h
+celery -A koala worker -Q $QUEUE --concurrency=1 --loglevel=info --pool threads -n ${QUEUE}_worker@%h
--- koala/celery.py
+++ koala/celery.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 from celery import Celery
 
@@ -7,6 +8,13 @@ from koala import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koala.settings')
 
 app = Celery('koala')
+
+if settings.ENV != 'local':
+    app.conf.broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_REQUIRED,
+        'ssl_ca_certs': settings.RABBITMQ_CA_CERT_PATH,
+    }
+
 
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.conf.result_backend = settings.CELERY_RESULT_BACKEND
