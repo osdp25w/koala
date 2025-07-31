a2b90abb08f6d8ebf2f6f978be3cba4e526ad155
Pony <pony@unnotech.com>
2025-07-31 23:50:00 +0800

    fix: remove unuse env

--- koala/celery.py
+++ koala/celery.py
@@ -8,19 +8,11 @@ from koala import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koala.settings')
 
 app = Celery('koala')
-
-if settings.ENV != 'local':
-    app.conf.broker_use_ssl = {
-        'ssl_cert_reqs': ssl.CERT_REQUIRED,
-        'ssl_ca_certs': settings.RABBITMQ_CA_CERT_PATH,
-    }
-
-
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.conf.result_backend = settings.CELERY_RESULT_BACKEND
 
 
 app.autodiscover_tasks()
 
-# 手動包含 MQTT 任務
+# mqtt is not a django app
 app.autodiscover_tasks(['koala.mqtt'])
