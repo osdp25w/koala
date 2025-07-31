31de319e46fb218ebc020b6171bc906cd8a823bc
Pony <pony@unnotech.com>
2025-08-01 00:23:54 +0800

    fix(celery): fix ssl cert

--- koala/celery.py
+++ koala/celery.py
@@ -12,6 +12,13 @@ app.conf.broker_url = settings.CELERY_BROKER_URL
 app.conf.result_backend = settings.CELERY_RESULT_BACKEND
 
 
+if settings.ENV != 'local':
+    app.conf.broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_REQUIRED,
+        'ssl_ca_certs': settings.RABBITMQ_CA_CERT_PATH,
+    }
+
+
 app.autodiscover_tasks()
 
 # mqtt is not a django app
